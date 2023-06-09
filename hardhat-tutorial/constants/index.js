70fdeaaa518be53828ff4b44c4560bf6457ca577
Santiago Vermeulen <48023484+svermen@users.noreply.github.com>
2023-06-09 10:23:58 -0300

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x6926Bf009241c3c76611918ebc44461F0c512Bc7";
+const WHITELIST_CONTRACT_ADDRESS = "0xD527378E4BB49d28C9fE15945D8d9c07c898C3A7";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-svermen.vercel.app/api/";
-
+//const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
